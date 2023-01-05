640baa7a46c6fff7e199b6ada8dcdf92d0ed89a7
luis <es.luiss89@gmail.com>
2023-01-05 14:53:22 -0300

    segunda modificacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 
+
 const Header = () => {
   return (
     <div className="header">
@@ -14,3 +15,4 @@ const Header = () => {
 };
 
 export default Header;
+
--- src/components/MiApi.js
+++ src/components/MiApi.js
@@ -1,27 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './MiApi.css';
 
 const MiApi = () => {
   const [indicador, setIndicador] = useState({});
   const [moneda, setMoneda] = useState('dolar');
   const [showPrePage, setShowPrePage] = useState(true);
+  const [resultados, setResultados] = useState([]); 
 
-  const obtenerIndicador = async (moneda) => {
+  // Envolver la función en useCallback
+  const obtenerIndicador = useCallback(async (moneda) => {
     try {
       const response = await fetch(`https://mindicador.cl/api/${moneda}`);
       const data = await response.json();
-      setIndicador(data.serie[0]);
+      setIndicador(data.serie[1]);
+      setResultados([data.serie[1]]);
       setTimeout(() => {
         setShowPrePage(false);
       }, 3000);
     } catch (error) {
       console.error(error);
     }
-  };
+  }, []); 
 
   useEffect(() => {
     obtenerIndicador(moneda);
-  }, [moneda]);
+  }, [moneda, obtenerIndicador]); 
 
   if (showPrePage) {
     return (
@@ -40,38 +43,42 @@ const MiApi = () => {
           id="moneda"
           value={moneda}
           onChange={(event) => {
-            setMoneda(event.target.value);
-            obtenerIndicador(event.target.value);
-          }}
-        >
-          <option value="uf">Unidad de Fomento</option>
-          <option value="ivp">Indice de Valor Promedio</option>
-          <option value="dolar">Dólar</option>
-          <option value="dolar_intercambio">Dólar Intercambio</option>
-          <option value="euro">Euro</option>
-          <option value="ipc">Indice de Precios al Consumidor</option>
-          <option value="utm">Unidad Tributaria Mensual</option>
-          <option value="imacec">Indice Mensual de Actividad Económica</option>
-          <option value="tpm">Tasa Política Monetaria</option>
-          <option value="libra_cobre">Libra de Cobre</option>
-          <option value="tasa_desempleo">Tasa de Desempleo</option>
-          <option value="bitcoin">Bitcoin</option>
-        </select>
-      </div>
-      <div className="mi-api card">
-        <div className="card-body">
-          <h2 className="card-title">Opcion Selecionada a Mostrar</h2>
-          <p className="card-text">Fecha: {indicador.fecha}</p>
-          <p className="card-text">Valor: ${indicador.valor}</p>
+              setMoneda(event.target.value);
+              obtenerIndicador(event.target.value);
+            }}
+          >
+            <option value="uf">Unidad de Fomento</option>
+            <option value="ivp">Indice de Valor Promedio</option>
+            <option value="dolar">Dólar</option>
+            <option value="dolar_intercambio">Dólar Intercambio</option>
+            <option value="euro">Euro</option>
+            <option value="ipc">Indice de Precios al Consumidor</option>
+            <option value="utm">Unidad Tributaria Mensual</option>
+            <option value="imacec">Indice Mensual de Actividad Económica</option>
+            <option value="tpm">Tasa Política Monetaria</option>
+            <option value="libra_cobre">Libra de Cobre</option>
+            <option value="tasa_desempleo">Tasa de Desempleo</option>
+            <option value="bitcoin">Bitcoin</option>
+          </select>
+        </div>
+        <div className="resultados">
+          {resultados.slice(-2).map((resultado, index) => (
+            <div className="resultado" key={index}>
+              <p>Consultas:</p>
+              <p>Fecha: {resultado.fecha}</p>
+              <p>Valor: ${resultado.valor}</p>
+            </div>
+          ))}
+        </div>
+        <div className="mi-api card">
+          <div className="card-body">
+            <h2 className="card-title">Opcion Selecionada a Mostrar</h2>
+            <p className="card-text">Fecha: {indicador.fecha}</p>
+            <p className="card-text">Valor: ${indicador.valor}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default MiApi;
-
-
-
-
-
+    );
+  };
+  
+  export default MiApi;
